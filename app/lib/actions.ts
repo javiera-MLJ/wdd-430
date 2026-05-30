cc36600be5d91c4ca163d2b56a6a602850981505
javiera-MLJ <lor24003@byui.edu>
2026-05-30 13:23:19 -0400

    chaging  throw error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -35,9 +35,7 @@ export async function createInvoice(formData: FormData) {
         `;
     } catch (error) {
         console.error(error);
-        return {
-            message: 'Database Error: Failed to Create Invoice.',
-        };
+        throw new Error('Database Error: Failed to Create Invoice.');
     }
 
     revalidatePath('/dashboard/invoices');
@@ -60,9 +58,8 @@ export async function updateInvoice(id: string, formData: FormData) {
             WHERE id = ${id}
         `;
     } catch (error) {
-
-        console.error(error);
-        return { message: 'Database Error: Failed to Update Invoice.' };
+    console.error(error);
+    throw new Error('Database Error: Failed to Update Invoice.');
     }
 
     revalidatePath('/dashboard/invoices');
